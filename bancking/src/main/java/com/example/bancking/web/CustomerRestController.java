a85b5ab20619d28fe81539b6aa8b558a836c9aff
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-05-18 11:28:39 +0100

    TpBankbackendSecurity2

--- bancking/src/main/java/com/example/bancking/web/CustomerRestController.java
+++ bancking/src/main/java/com/example/bancking/web/CustomerRestController.java
@@ -6,6 +6,7 @@ import com.example.bancking.exceptions.CustomerNotFoundException;
 import com.example.bancking.services.BankAccountService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,28 +18,39 @@ import java.util.List;
 public class CustomerRestController {
     private BankAccountService bankAccountService;
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCORE_USER')")
     public List<CustomerDTO> customers(){
         return bankAccountService.listCustomer();
     }
     @GetMapping("/customers/search")
+    @PreAuthorize("hasAuthority('SCORE_USER')")
+
     public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "")String keyword){
         return bankAccountService.searchCustomers("%"+keyword+"%");
     }
 @GetMapping("/customers/{id}")
+@PreAuthorize("hasAuthority('SCORE_USER')")
+
     public CustomerDTO getCustomer( @PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
 return  bankAccountService.getCustomer(customerId);
 }
 
     @PostMapping("/customers")
+    @PreAuthorize("hasAuthority('SCORE_ADMIN')")
+
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO )  {
         return  bankAccountService.saveCustomer(customerDTO);
     }
 @PutMapping ("/customers/{customerId}")
+@PreAuthorize("hasAuthority('SCORE_ADMIN')")
+
     public CustomerDTO updateCustomer(@PathVariable Long customerId,@RequestBody CustomerDTO customerDTO){
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
 }
 @DeleteMapping ("/customers/{customerId}")
+@PreAuthorize("hasAuthority('SCORE_ADMIN')")
+
     public void deletCustomer(@PathVariable Long customerId){
         bankAccountService.deletCustomer(customerId);
 }
